ee1bfc7192a2e6856227626d5ca2a47053529df3
volk0423 <154359973+volk0423@users.noreply.github.com>
2025-02-09 11:29:21 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Спасибо за ваше сообщение! Мы свяжемся с вами в ближайшее время.');
+    this.reset();
+});
